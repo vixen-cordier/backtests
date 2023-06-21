446f8d45d21ffd85d2072bfc77040dba9bdb6d86
VCO <vixen.cordier.13@gmail.com>
2023-06-21 23:46:02 +0200

    form class and dash to streamlit ^^

--- app.py
+++ app.py
@@ -1,56 +1,12 @@
-from dash import dash, Dash, html, dcc, callback, Output, Input
-import dash_bootstrap_components as dbc
-
-from datetime import datetime 
-import plotly.graph_objects as go
 import pandas as pd
+import streamlit as st
+from typing import List
+st.set_page_config(layout="wide", page_title="Portfolio Backtest")
+import plotly.graph_objects as go
 
-from utils.portfolio import Portfolio
-
-    
-# Layout construction
-# =============================
-app = Dash(__name__, use_pages=True, external_stylesheets=[dbc.themes.CYBORG])
-
-app.layout = html.Div([
-    dbc.Navbar(
-        dbc.Container([
-            html.A(
-                dbc.Row([
-                    dbc.Col(html.Img(src='assets/C.png', height="30px")),
-                    dbc.Col(dbc.NavbarBrand("Portfolio Backtest", className="ms-2")),
-                ]),
-                href="/home",
-                style={"textDecoration": "none"},
-            ),
-            dbc.NavbarToggler(id="navbar-toggler", n_clicks=0),
-        ]),
-        color="dark",
-        dark=True,
-    ),
-    dash.page_container,
-])
+import form
 
 
-# # Callbacks definition
-# # =============================
-# @callback(
-#     Output('portfolio-graph', '_'),
-#     Input('ticker-input', 'ticker')
-# )
-# def edit_form(portfolio):
-#     return None
+portfolios = form.read_forms()
 
-
-# @callback(
-#     Output('portfolio-graph', '_'),
-#     Input('ticker-input', 'ticker')
-# )
-# def view_result(portfolio):
-#     return None
-
-
-# main
-# =============================
-if __name__ == '__main__':
-    app.run_server(debug=True)
+st.title("Backtest portfolio")
